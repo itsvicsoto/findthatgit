74776cb5eb36518a5777f24104b0b44f1c64398f
itsvicsoto <itsvicsoto@gmail.com>
2018-10-17 14:09:07 +0800

    tidy

--- server/server.js
+++ server/server.js
@@ -1,37 +1,12 @@
 var express = require('express');
-var https = require('https');
 var http = require('http');
 var app = express();
 
 // Other Libraries
 var path = require('path');
-var wkhtmltopdf = require('wkhtmltopdf');
-var fs = require('fs');
 
 var bodyParser = require('body-parser');
-var multer = require('multer');
-var upload = multer();
-var asyncLib = require('async');
-var url = require('url');
 
-var request = require('request');
-var schedule = require('node-schedule');
-var querystring = require('querystring');
-
-// Engine Dependencies
-var consolidate = require('consolidate');
-
-// Logger
-var moment = require('moment');
-var morgan = require('morgan');
-var winston = require('winston');
-var rfs = require('rotating-file-stream');
-var rotateTransport = require('winston-logrotate');
-// Log level
-var level = process.env.LOG_LEVEL || 'debug';
-
-// command check
-var commandExistsSync = require('command-exists').sync;
 
 // Load environment based on NODE_ENV
 var environment;
@@ -63,35 +38,14 @@ app.use('/', express.static(path.resolve(__dirname, '../app/dashboard')));
 app.use('/vendor', express.static(path.resolve(__dirname, '../bower_components')));
 
 var register = function(app) {
-
-  var wkhtmltopdfStatus = false;
-
-  if (commandExistsSync('wkhtmltopdf')) {
-    wkhtmltopdfStatus = true;
-  }
-
-  // logger.info(`======================================================`);
-  logger.info(`SERVER STARTING...`);
-  // logger.info(`------------------------------------------------------`)
-  logger.info(`LISTENING ON PORT : ${environment.PORT}`)
-  // logger.info(`------------------------------------------------------`)
-
-  // logger.info(`------------------------------------------------------`)
-  if (!wkhtmltopdfStatus) {
-    logger.error(`URGENT :: WKHTMLTOPDF is missing on the command line`)
-    // logger.info(`------------------------------------------------------`)
-  }
-  logger.info(`SERVER STARTED`);
-  // logger.info(`======================================================`)
-
+  console.info(`SERVER STARTING...`);
+  console.info(`LISTENING ON PORT : ${environment.PORT}`)
+  console.info(`SERVER STARTED`);
 
   // Catching All Pages that arent found
   app.use('*', function(req, res, next) {
-
     return next();
-
   });
-
 
   // ROUTES
   require('./routes/dashboard')(app);
